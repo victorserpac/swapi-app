1fe50bc083e4d42c8bfca39a6f2cb9bf893bc490
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2017-09-18 02:03:48 -0300

    Melhorando header e footer da aplicação

--- src/app/movie-listing/movie-listing.component.ts
+++ src/app/movie-listing/movie-listing.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { MovieService } from '../movie/movie.service';
-// import { MovieComponent } from '../movie/movie.component';
 
 @Component({
   selector: 'movie-listing',
@@ -21,7 +20,6 @@ export class MovieListingComponent {
     service.list()
       .then(movies => {
         this.movies = movies;
-        console.log(movies);
       })
       .catch(msg => this.message = msg);
   }
